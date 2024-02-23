06f64f632fce37b82d0f97d199222f5b11f1115e
Edgar QS <34812060+edgarqs@users.noreply.github.com>
2024-02-23 01:02:27 +0100

    Update

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ function connectToFiveM() {
 }
 
 document.oncontextmenu = function(){return false} // Deshabilitar el click derecho
+
+
+// Función para cambiar el fondo de pantalla
+
+document.addEventListener('DOMContentLoaded', function() {
+    const wallpapers = [
+        //'styles/img/wallpapers/1.jpg',
+        'styles/img/wallpapers/2.jpg',
+        'styles/img/wallpapers/3.jpg',
+        'styles/img/wallpapers/4.png',
+        'styles/img/wallpapers/5.jpg',
+        'styles/img/wallpapers/6.jpg',
+        'styles/img/wallpapers/7.jpg',
+        'styles/img/wallpapers/8.jpg'
+    ];
+
+    // Precargar imágenes
+    wallpapers.forEach(src => {
+        const img = new Image();
+        img.src = src;
+    });
+
+    let currentWallpaperIndex = 1;
+
+    function changeBackground() {
+        document.body.style.backgroundImage = `url('${wallpapers[currentWallpaperIndex]}')`;
+        currentWallpaperIndex = (currentWallpaperIndex + 1) % wallpapers.length; // Avanzar al siguiente wallpaper
+    }
+
+    // Cambiar el fondo cada 3 segundos
+    setInterval(changeBackground, 3000);
+});
